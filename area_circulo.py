f43919cfd25940599aeb506a63a87cbaae3c75a4
JOÃO VICTOR <joaovictornoliveira@outlook.com>
2022-03-24 17:44:12 -0300

    Atividades 33 e 34

---
+++ area_circulo.py
@@ -0,0 +1,3 @@
+
+raio = float(input("Informe o valor do raio em centímetros:"))
+print(f'A área do círculo é de {3.141592 * raio * raio} centímetros.')
---
+++ area_quaadrado.py
@@ -0,0 +1,2 @@
+lado = int(input("Insira o valor do lado do quadrado em centímetros:"))
+print(f'Área do quadrado é de {lado * lado} centímetros.')
