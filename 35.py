089b848fb457098819a6ccd3b4767871193fd5d4
JOÃO VICTOR <joaovictornoliveira@outlook.com>
2022-04-05 10:31:39 -0300

    Correções 35 e 40

--- 35.py
+++ 35.py
@@ -1,6 +1,4 @@
-# A fórmula oferecida pelo enunciado está incorreta, a fórmula correta é: Hipotenusa = a² + b²
-
 cat1 = int(input("Informe o valor do primeiro cateto: "))
 cat2 = int(input("Informe o valor do segundo cateto: "))
 
-print(f'O valor da hipotenusa é {cat1 * cat1 + cat2 * cat2}')
+print(f'O valor da hipotenusa é {(cat1 * cat1 + cat2 * cat2) ** 0.5}')
