283ba067a0bb1b9e6257a8db2c723cae6f5dc132
João Victor Nascimento <66145032+joaovictorjvn@users.noreply.github.com>
2022-04-19 14:22:25 -0300

    49 - Otimização final(Execução perfeita)

--- 49.py
+++ 49.py
@@ -1,19 +1,23 @@
 hor = int(input("Informe a atual hora: "))
-min = int(input("Informe o atual minuto: "))
+minu = int(input("Informe o atual minuto: "))
 seg = int(input("Informe o atual segundo: "))
 durh = int(input("Informe as horas de duração do evento: "))
 durm = int(input("Informe os minutos de duração do evento(Acompanha as horas inseridas anteriormente): "))
 durs = int(input("Informe os segundos de duração do evento(Acompanha as horas e minutos inseridos anteriormente): "))
-if hor + durh >= 24 and min + durm >= 60 and seg + durs >= 60:
-    print(f'O horário de termino da reunião é as {((hor + durh) - 24) + 1}:{((min + durm) - 60) + 1}:'
-          f'{(seg + durs) - 60}')
-elif hor + durh >= 24 and min + durm >= 60:
-    print(f'O horário de termino da reunião é as {((hor + durh) - 24) + 1}:{(min + durm) - 60}:{seg + durs}')
-elif min + durm >= 60 and seg + durs >= 60:
-    print(f'O horário de termino da reunião é as {hor + durh+ 1}:{((min + durm) - 60) + 1}:{(seg + durs) - 60}')
-elif hor + durh >= 24:
-    print(f'O horário de termino da reunião é as {(hor + durh) - 24}:{min + durm}:{seg + durs}')
-elif min + durm >= 60:
-    print(f'O horário de termino da reunião é as {hor + durh + 1}:{(min + durm) - 60}:{seg + durs}')
-elif seg + durs >= 60:
-    print(f'O horário de termino da reunião é as {hor + durh}:{min + durm + 1}:{(seg + durs) - 60}')
+horenc = hor + durh
+minuenc = minu + durm
+segenc = seg + durs
+if hor >= 24 or minu >= 60 or seg >= 60:
+    print("Dados inválidos inseridos.")
+else:
+    print(f'O evento se iniciará as {hor}:{minu}:{seg}')
+    if segenc >= 60:
+        segenc = segenc - 60
+        minuenc = minuenc + 1
+    if minuenc >= 60:
+        minuenc = minuenc - 60
+        horenc = horenc + 1
+    if horenc >= 24:
+        horenc = horenc - 24
+    print(f'O evento terá a duração de {durh} horas, {durm} minutos e {durs} segundos, sendo encerrado as '
+          f'{horenc}:{minuenc}:{segenc}')
